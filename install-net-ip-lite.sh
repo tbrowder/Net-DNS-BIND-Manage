3791f3839b59922334113197eb2e6929d8b96947
Tom Browder <tom.browder@gmail.com>
2016-10-02 15:00:08 -0500

    timotimo says no cd required

--- install-net-ip-lite.sh
+++ install-net-ip-lite.sh
@@ -1,8 +1,12 @@
 #!/bin/sh
 git clone https://github.com/tbrowder/Net-IP-Lite-Perl6.git
-cd Net-IP-Lite-Perl6
+
+#cd Net-IP-Lite-Perl6
+
 # the following two lines are for added info in the travis build log"
 TD=`pwd`
 echo "=== now working in dir '$TD' ==="
 # the coup de grace:
-panda install .
+
+panda install Net-IP-Lite-Perl6
+#panda install .
